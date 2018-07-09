e2a0f46923fa76c3afcade01ee64003684d0b3ab
Carraway <carraways14@refmac6.intra.ecu.edu>
2018-07-09 16:10:39 -0400

    Dice

---
+++ Tools.js
@@ -0,0 +1,14 @@
+
+function twentyd(){
+document.getElementById("twenty").innerHTML = Math.floor((Math.random() * 20) + 1);
+}
+
+function tend(){
+document.getElementById("ten").innerHTML = Math.floor((Math.random() * 10) + 1);
+}
+
+function sixd(){
+document.getElementById("six").innerHTML = Math.floor((Math.random() * 6) + 1);
+}
+
+alert("Alert!!!");
